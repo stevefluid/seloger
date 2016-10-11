3f3da3575a3d4c42ab42fe207575e997c0c35e2c
Steven LE BOLLOCH <steven.lebolloch@seloger.com>
2016-10-11 14:54:38 +0200

    first mod and add by stvn : new js file and main js mods

--- WebApplication/assets/js/main.js
+++ WebApplication/assets/js/main.js
@@ -1,3 +1,8 @@
+/**
+ * Mod by Steven : add jsDoc block in existing file
+ * + store console.log string value in logMessage var
+ */
 (function() {
-    console.log('I\'m an Amazing WebApp');
+	var logMessage = 'I\'m an Amazing WebApp';
+    console.log( logMessage );
 })();
---
+++ WebApplication/assets/js/stvn.1.js
@@ -0,0 +1,8 @@
+/**
+ * Add by Steven : add new js file
+ * + store console.log string value in logMessage var
+ */
+(function() {
+	var stvnLogMessage = 'I\'m an new js module added by Steven';
+    console.log( stvnLogMessage );
+})();
